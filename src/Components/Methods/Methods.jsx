7aca13187c3f2d23c4b3e74f2e02e7fee86f9650
Марк Неловка <87484591+Mark-Nelovka@users.noreply.github.com>
2023-02-03 01:47:54 +0200

    Added new function for test

--- src/Components/Methods/Methods.jsx
+++ src/Components/Methods/Methods.jsx
@@ -1,18 +1,30 @@
 export function Methods() {
   // Test([13, 215, 136, 54, 18, 0]);
+  // reverseWords("The quick brown fox jumps over the lazy dog.");
+  // getCount("abracadabra"); // 5
+  createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]); // "(123) 456-7890"
 
   return <div>Methods</div>;
 }
 
-// function Test(item) {
-//   for (let i = 0; i < item.length; i++) {
-//     for (let j = i; j < item.length; j++) {
-//       if (item[i] > item[j]) {
-//         let t = item[i];
-//         item[i] = item[j];
-//         item[j] = t;
-//       }
+// function reverseWords(str) {
+//   let reversedWord = "";
+//   let reversedStr = "";
+//   for (let i = 0; i < str.length; i++) {
+//     if (str[i] !== " ") {
+//       reversedWord = str[i] + reversedWord;
+//     } else {
+//       reversedStr += reversedWord + " ";
+//       reversedWord = "";
 //     }
 //   }
-//   return item;
+//   console.log(reversedStr + reversedWord);
+//   return reversedStr + reversedWord;
 // }
+
+function createPhoneNumber(numbers) {
+  const copiArr = numbers;
+  const code = copiArr.slice(0, 3).join("");
+  const number = copiArr.slice(3, 6).join("");
+  return console.log(`(${code}) ${number}-${copiArr.slice(6, 10).join("")}`);
+}
